03b9fd1a6daa5e736fc0abfcbc19acd644fa26e1
Marcin Przewięźlikowski <m.przewie@gmail.com>
2024-07-01 13:27:20 +0200

    named ds root

--- datasets.py
+++ datasets.py
@@ -7,27 +7,24 @@ from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder, STL10
 from torchvision.transforms import ToTensor, Compose, Normalize, transforms
 
-SIMPLE_DS_ROOT = "data"
 def get_datasets(args, *, stl_train_ctx: Literal["train", "test"] = None) -> Tuple[Dataset, Dataset, dict]:
     if args.ds == "cifar10":
-        train_dataset = torchvision.datasets.CIFAR10(SIMPLE_DS_ROOT, train=True, download=True,
+        root = args.named_ds_root / "cifar10"
+        train_dataset = torchvision.datasets.CIFAR10(root, train=True, download=True,
                                                      transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-        val_dataset = torchvision.datasets.CIFAR10(SIMPLE_DS_ROOT, train=False, download=True,
+        val_dataset = torchvision.datasets.CIFAR10(root, train=False, download=True,
                                                    transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-        # imsize_kwargs = dict(
-        #     image_size=32,
-        #     patch_size=2,
-        # )
+
     elif args.ds == "cifar100":
-        train_dataset = torchvision.datasets.CIFAR100(SIMPLE_DS_ROOT, train=True, download=True,
+        root = args.named_ds_root / "cifar100"
+        train_dataset = torchvision.datasets.CIFAR100(root, train=True, download=True,
                                                       transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-        val_dataset = torchvision.datasets.CIFAR100(SIMPLE_DS_ROOT, train=False, download=True,
+        val_dataset = torchvision.datasets.CIFAR100(root, train=False, download=True,
                                                     transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-        # imsize_kwargs = dict(
-        #     image_size=32,
-        #     patch_size=2,
-        # )
+
     elif args.ds == "stl10":
+        root = args.named_ds_root / "stl10"
+
         mean = torch.tensor([0.43, 0.42, 0.39])
         std = torch.tensor([0.27, 0.26, 0.27])
 
@@ -49,13 +46,8 @@ def get_datasets(args, *, stl_train_ctx: Literal["train", "test"] = None) -> Tup
         else:
             assert False, f"{stl_train_ctx=}"
 
-        train_dataset = STL10("/shared/sets/datasets/vision/stl10", split=train_split, transform=transform_train, download=True)
-        val_dataset = STL10("/shared/sets/datasets/vision/stl10", split='test', transform=transform_val, download=True)
-        # imsize_kwargs = dict(
-        #     image_size=args.resolution,
-        #     patch_size=args.patch_size,
-        # )
-
+        train_dataset = STL10(root, split=train_split, transform=transform_train, download=True)
+        val_dataset = STL10(root, split='test', transform=transform_val, download=True)
 
     else:
         transform_train = transforms.Compose([
@@ -70,10 +62,7 @@ def get_datasets(args, *, stl_train_ctx: Literal["train", "test"] = None) -> Tup
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         train_dataset = ImageFolder(os.path.join(args.ds, 'train'), transform=transform_train)
         val_dataset = ImageFolder(os.path.join(args.ds, 'val'), transform=transform_val)
-        # imsize_kwargs = dict(
-        #     image_size=args.resolution,
-        #     patch_size=args.patch_size,
-        # )
+
 
     return train_dataset, val_dataset #, imsize_kwargs
 
